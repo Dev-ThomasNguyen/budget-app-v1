640749a9d1386c5d10fac42b94a35e57a6c62866
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-14 00:00:28 -0400

    first stage

--- app.js
+++ app.js
@@ -3,19 +3,45 @@ var budgetController = (function() {})();
 
 // UI CONTROLLER
 var UIController = (function() {
-  // Some code
+  var DOMstrings = {
+    inputype: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputype).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    // 1. Get the fieled input data
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 5. Display the budget on the UI
-  });
+  };
+
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(e) {
-    console.log(e);
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
   });
 })(budgetController, UIController);
